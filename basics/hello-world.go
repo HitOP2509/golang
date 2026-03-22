393f25c1ffc1188c6065717483a30a7813a92384
Rohit Singh <hitop2509@gmail.com>
2026-03-22 19:52:53 +0530

    add: hello world code

---
+++ basics/hello-world.go
@@ -0,0 +1,8 @@
+package basics
+
+import "fmt"
+
+func HelloWorld() {
+	fmt.Println("Hello, World!")
+
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/HitOP2509/golang.git/basics"
+
+func main() {
+	basics.HelloWorld()
+}
