02e15b3fb4d642d77b0ff27a9189f10e10fd3f28
Rohit Singh <hitop2509@gmail.com>
2026-06-01 11:59:08 +0530

    feat: struct with struct embedding/composition, and striuct Pointer & Value receivers methods

--- basics/10_structs/main.go
+++ basics/10_structs/main.go
@@ -2,48 +2,80 @@ package main
 
 import "fmt"
 
+/********** Pointer embedding **********/
+// Pointer embedding in Go is conceptually similar to using a foreign key in DB.
+// Both mean: “Don’t duplicate the full data. Keep a reference to the original thing.”
+// If the original data changes, everyone referring to it sees the updated data.
+
+// DB foreign key -> stores an ID/reference to another row.
+// Go pointer     -> stores a memory address/reference to another object.
+
+/********** Value embedding **********/
+// Value embedding in Go means the struct keeps its own copy of the data.
+// So even if the original data changes later, this struct still has the old copied data.
+
+// This is useful when you want a snapshot-like behavior and do not want the data to be updated when the original data changes.
+
+// Struct embedding / Struct composition
 type User struct {
-	name string
-	age  int
+	name    string
+	address string
 }
 
-type TimeEntry struct {
-	TaskName string
-	Duration int
-	Date     string
+type OrderDetails struct {
+	id              string
+	productId       string
+	deliveryAddress string
+	status          string
+}
+type Order struct {
+	OrderDetails //Value embedding
+	*User        //Pointer embedding
 }
 
-/*
- *	Method with value receiver.
- *	`user User` works like `this` in JS/TS, but Go passes a copy of the User.
- *	Use this when the method only reads data and does not modify the original struct.
- */
-func (user User) GetUserName() string {
-	return "User name is: " + user.name + "."
+// Value receivers methods
+// You can use value when just reading it
+func (o Order) GetOrderDeliveryInfo() string {
+	return fmt.Sprintf("Order status for %s is: %s", o.name, o.status)
 }
 
-/*	Method with pointer receiver.
- *	`u *User` works like `this`, but points to the original User.
- *	Use this when the method needs to modify the original struct or avoid copying large structs.
- */
-func (u *User) SetName(name string) {
-	u.name = name
+// Pointer receiver method
+// Use pointer receiver when the method needs to modify the original struct.
+func (o *Order) UpdateDeliveryStatus(status string) {
+	o.status = status
 }
 
 func main() {
-	u := User{
-		name: "Rohit Singh",
-		age:  18,
+	user := &User{
+		name:    "Rohit Singh",
+		address: "Chinarpark, Kolkata - 700059",
 	}
-	task := TimeEntry{
-		TaskName: "Coding",
-		Duration: 60,
-		Date:     "2026-12-12",
-	}
-	fmt.Println(u.GetUserName())
-	u.SetName("Mr. Rohit Singh")
-	fmt.Println(u.GetUserName())
-	fmt.Println(u.name)
 
-	fmt.Println(task.TaskName)
+	order := OrderDetails{
+		id:              "1",
+		productId:       "1",
+		deliveryAddress: "Chinarpark, Kolkata - 700059",
+		status:          "pending",
+	}
+
+	newOrder := Order{
+		User:         user,
+		OrderDetails: order,
+	}
+	newOrder2 := Order{
+		User:         user,
+		OrderDetails: order,
+	}
+
+	// This changes the original object and reflects on both the objects newOrder and newOrder2
+	newOrder2.User.name = "Mr. Rohit Singh"
+
+	//This change reflects inside only newOrder2
+	newOrder2.UpdateDeliveryStatus("delivered")
+
+	fmt.Println(newOrder.GetOrderDeliveryInfo())  //Order status for Mr. Rohit Singh is: pending
+	fmt.Println(newOrder2.GetOrderDeliveryInfo()) //Order status for Mr. Rohit Singh is: delivered
+
+	// WE CAN SEE THE NAME UPDATED IN BOTH THE OBJECTS DESPITE CHANGING IT IN ONLY 1 BUT DELIVERY STATUS IS DIFFERENT
+
 }
